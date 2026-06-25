e5b7136e260e5fa8f8e851feb2332ed91a7af37e
415-26-phiphattharaphon <56704@yupparaj.ac.th>
2026-06-25 09:07:02 +0700

    Create  digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("My Digital profile")
+
+st.header("แนะนำตัว")
+st.write("นางสาวภิภัทรภรณ์ สมพันธ์")
+st.write("ชื่อเล่น ครีม")
+st.write("เลขที่26 ม.4/15")
+
+st.header("เป้าหมายและสิ่งที่อยากทำในอนาคต")
+st.write("เป้าหมาย: **อยากเรียนต่อด้านภาษาให้เก่งๆ")
+st.write("สิ่งที่อยากทำในอนาคต: แอร์โฮสเตส (Flight Attendant)")
